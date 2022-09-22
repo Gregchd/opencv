18735ba17a6cb9d557a833726a475c96e1d4985a
Gregchd <gregorygnr@gmail.com>
2022-09-21 19:18:31 -0500

    contour detection

--- contourdetection.py
+++ contourdetection.py
@@ -1,26 +1,24 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('Photos/cats.jpg')
 cv.imshow('Cats', img)
 
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow('Blank', blank)
+
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-canny = cv.Canny(img, 125, 175)
-cv.imshow('Canny', canny)
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+cv.imshow('thresh', thresh)
 
 contours, hierarchies = cv.findContours(
-    canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-
-#cv.findContours(img, contours.method,aproximation.method)
-"""contours.method
-cv.RETR_LIST = todos los contornos 
-cv.RETR_TREE = all the contours that are in a hierarchical system
-cv.RETR_EXTERNAL = contornos externos """
-""" aproximation.method
-cv.CHAIN_APPROX_NONE = null
-cv.CHAIN_APPROX_SIMPLE = simplifica los contornos """
+    thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
 print(f'{len(contours)} contours found')
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 1)
+cv.imshow('Contours Drawn', blank)
 
 cv.waitKey(0)
