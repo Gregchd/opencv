8df1f1444c5dfc0b470553b63754f9fdf4873d16
Gregchd <gregorygnr@gmail.com>
2022-09-23 13:12:46 -0500

    color channels

--- splitmerge.py
+++ splitmerge.py
@@ -3,3 +3,26 @@ import numpy as np
 
 img = cv.imread('Photos/Park.jpg')
 cv.imshow('Boston', img)
+
+b, g, r = cv.split(img)
+
+# nos entrega los canales de colores, pero en grayscale
+cv.imshow('blue', b)
+cv.imshow('green', g)
+cv.imshow('red', r)
+
+merged = cv.merge([b, g, r])
+cv.imshow('merge', merged)
+
+# para poder ver los canales de colores tenemos que reconstruir la imagenes
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+blue = cv.merge([b, blank, blank])
+green = cv.merge([blank, g, blank])
+red = cv.merge([blank, blank, r])
+
+cv.imshow('cblue', blue)
+cv.imshow('cgreeen', green)
+cv.imshow('cred', red)
+
+cv.waitKey(0)
